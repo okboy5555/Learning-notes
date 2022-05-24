f0dd4a58050870d2be833d30f3a8f40aae9c21ae
yuhuai <miaojiasheng.mjs@alibaba-inc.com>
2022-05-24 21:56:58 +0800

    feat: 新增常见算法

---
+++ coding/二分/二分法.js
@@ -0,0 +1,21 @@
+function BinarySearch(arr, target) {
+  if (arr.length <= 1) return -1
+  // 低位下标
+  let lowIndex = 0
+  // 高位下标
+  let highIndex = arr.length - 1
+
+  while (lowIndex <= highIndex) {
+      // 中间下标
+      const midIndex = Math.floor((lowIndex + highIndex) / 2)
+      if (target < arr[midIndex]) {
+          highIndex = midIndex - 1
+      } else if (target > arr[midIndex]) {
+          lowIndex = midIndex + 1
+      } else {
+          // target === arr[midIndex]
+          return midIndex
+      }
+  }
+  return -1
+}
