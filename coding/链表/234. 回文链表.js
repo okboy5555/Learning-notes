1c2ee43f5cdacbe844dd93186c7c3284372712a8
yuhuai <miaojiasheng.mjs@alibaba-inc.com>
2022-09-23 14:54:17 +0800

    feat: 链表

---
+++ coding/链表/234. 回文链表.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ const reverseList = (head) => {
+  let prev = null;
+  let curr = head;
+  while (curr !== null) {
+    let nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
+  return prev;
+}
+
+const endOfFirstHalf = (head) => {
+  let fast = head;
+  let slow = head;
+  while (fast.next !== null && fast.next.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+  return slow;
+}
+
+var isPalindrome = function (head) {
+  if (head == null) return true;
+
+  // 找到前半部分链表的尾节点并反转后半部分链表
+  const firstHalfEnd = endOfFirstHalf(head);
+  const secondHalfStart = reverseList(firstHalfEnd.next);
+
+  // 判断是否回文
+  let p1 = head;
+  let p2 = secondHalfStart;
+  let result = true;
+  while (result && p2 != null) {
+    if (p1.val != p2.val) result = false;
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  // 还原链表并返回结果
+  firstHalfEnd.next = reverseList(secondHalfStart);
+  return result;
+};
+
+// 时间复杂度O(n)
+// 空间复杂度O(1)
